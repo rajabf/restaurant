fb301996bb8db11c632ec5000362bdac0ff0b3ea
Rajab <rajabmuzaffarov@yahoo.com>
2024-06-09 00:54:48 +0900

    meal-fixed-for-test-v1

--- js/meal.js
+++ js/meal.js
@@ -7,7 +7,8 @@ function renderMeals(meals) {
 
     meals.forEach(meal => {
         const params = new URLSearchParams({ meal: meal.strCategory})
-        const url = `https://mealsbyrajab.netlify.app/?${params}`
+        // const url = `https://mealsbyrajab.netlify.app/?${params}`
+        const url = `https://mealsbyrajab.netlify.app/meal.html?${params}`
 
         let li = document.createElement('li')
         li.setAttribute('class','list-group-item col-sm-12 col-lg-4 card')
